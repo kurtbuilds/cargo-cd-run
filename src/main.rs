06a323d9a1a4d5ff84482196ca59a1e6c73837f2
Kurt Wolf <kurtwolfbuilds@gmail.com>
2024-09-28 11:23:06 -0700

    0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::env::{set_current_dir, vars};
-use std::process::Command;
-use clap::{Parser, Subcommand};
 use anyhow::Result as AnyResult;
-use std_ext::CommandExt;
+use clap::Parser;
+use std::env::{args_os, set_current_dir};
+use std::process::Command;
+use std_ext::{CommandExt, OutputExt};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -14,7 +14,9 @@ struct Args {
 
 
 fn main() -> AnyResult<()> {
-    let cli = Args::parse();
+    let args = args_os().skip(1);
+    let cli = Args::parse_from(args);
+    dbg!(&cli);
 
     let cargo_args;
     let target_args;
@@ -30,9 +32,18 @@ fn main() -> AnyResult<()> {
         .arg("build")
         .args(cargo_args)
         .run()?;
+    let target_dir = std::env::var("CARGO_TARGET_DIR");
+    let target_dir = target_dir.as_deref().unwrap_or("target");
+
+    let find = format!("fd . {target_dir}/debug {target_dir}/release -t x --maxdepth 1 -X ls -t | head -n1");
+    let bin = Command::new("/bin/bash")
+        .args(&["-c", &find])
+        .output()?;
+    let bin = bin.stdout().trim_end();
+
     set_current_dir(cli.dir)?;
-    let bin_dir = std::env::var("CARGO_TARGET_DIR").as_deref().unwrap_or("target");
-    dbg!(vars());
-    // Command::new("")
+    Command::new(bin)
+        .args(target_args)
+        .run()?;
     Ok(())
 }
